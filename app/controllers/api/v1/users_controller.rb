e4f928f6be7c676fb8d5cac707f7368ab6110ec6
PEILI JIANG <1281277805@qq.com>
2019-04-24 19:10:02 +0800

    openid

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,37 @@
 class Api::V1::UsersController < Api::V1::BaseController
   before_action :set_user, only: [:show, :update, :destroy]
+  protect_from_forgery with: :null_session
+
+  def login
+    code = params[:code]
+    if params[:code].nil?
+      a = token_params
+      authen = a['token']
+      openid = decode(authen)
+      render json: {
+        status: 200
+      }
+    else
+      token = RestClient.get("https://api.weixin.qq.com/sns/jscode2session?appid=wx9907b4461ceeeee9&secret=f1df82dee8dfa9232df22129c4747846&js_code=#{params[:code]}&grant_type=authorization_code")
+      openid = JSON.parse(token)['openid']
+      payload = { token: openid }
+
+      @user = User.new
+      @user.username = openid
+      @user.save
+
+      authen = JWT.encode payload, nil, 'none'
+      render json: {
+        authen: authen
+      }
+    end
+  end
+
+  def decode(token)
+    t = JWT.decode token, nil, false
+    t[0]['token']
+  end
+
   def index
     @users = User.all
     render json: @users
@@ -19,6 +51,9 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def create
     @user = User.new(user_params)
+    # payload = { data:@user.id }
+    # token = JWT.encode payload, nil, "none"
+    # @user.token = token
     if @user.save
       render json: @user, status: :created
     else
@@ -43,5 +78,9 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def render_error
     render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def token_params
+    params.require(:tokens).permit(:token)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
         resources :dogs
         resources :appointments
       end
+      post "/login", to: "users#login", as: :login
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
